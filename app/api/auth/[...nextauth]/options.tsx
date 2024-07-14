575720d43df726cbd9f76cbcdb269814b6db0b6e
Mikey <pride81_@hotmail.com>
2024-07-15 03:25:32 +0900

    Add create user10

--- app/api/auth/[...nextauth]/options.tsx
+++ app/api/auth/[...nextauth]/options.tsx
@@ -29,13 +29,13 @@ export const options = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: any; user: any }) {
       if (user) {
         token.role = user.role;
       }
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: any; token: any }) {
       if (session?.user) {
         session.user.role = token.role;
       }
