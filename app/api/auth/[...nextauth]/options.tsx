75fa9d06f1eec055e1bbcedccfd93fb47379e102
Mikey <pride81_@hotmail.com>
2024-07-15 03:05:15 +0900

    Add create user5

--- app/api/auth/[...nextauth]/options.tsx
+++ app/api/auth/[...nextauth]/options.tsx
@@ -3,6 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const options = {
   providers: [
     GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
       profile(profile) {
         console.log("Profile Google", profile);
 
@@ -12,22 +14,27 @@ export const options = {
         }
 
         return {
-          ...profile,
+          // ...profile,
           id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
           role: userRole,
         };
       },
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_Secret,
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      if (user) token.role = user.role;
+      if (user) {
+        token.role = user.role;
+      }
       return token;
     },
     async session({ session, token }) {
-      if (session?.user) session.user.role = token.role;
+      if (session?.user) {
+        session.user.role = token.role;
+      }
       return session;
     },
   },
--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth";
 import { options } from "./options";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string | null; // Add role property
+    };
+  }
+}
+
 const handler = NextAuth(options);
 export { handler as GET, handler as POST };
